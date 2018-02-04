23ab84de002ff4b4d0d0379ddab5b591850d2e39
ppeczek <pawel.m.peczek@gmail.com>
2018-02-04 01:56:18 +0100

    Creating basic WebSocket communication

---
+++ AskSpeakerServer/AskSpeakerServer/BackEnd/ClientRequests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using WebSocketSharp.Server;
+
+namespace AskSpeakerServer.BackEnd {
+	public class ClientRequests : WebSocketBehavior {
+		protected override void OnOpen () {
+			Console.WriteLine ("Sending request!");
+			foreach (IWebSocketSession s in Sessions.Sessions) {
+				if (s != this) {
+					s.Context.WebSocket.SendAsync ($"New client arrives! ID: {this.ID}", NullAction);
+				}
+			}
+			SendAsync ("Welcome!", NullAction);
+		}
+
+		private void NullAction(bool d){
+			Console.WriteLine ($"Completed with: {d}");
+		}
+	}
+}
+
--- AskSpeakerServer/AskSpeakerServer/Program.cs
+++ AskSpeakerServer/AskSpeakerServer/Program.cs
@@ -3,8 +3,12 @@ using System.Data;
 using MySql.Data.MySqlClient;
 using System.Linq;
 using AskSpeakerServer.EntityFramework;
+using AskSpeakerServer.BackEnd;
 using System.Security.Cryptography;
 using System.Text;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+using System.Security.Cryptography.X509Certificates;
 
 namespace AskSpeakerServer
 {
@@ -12,62 +16,36 @@ namespace AskSpeakerServer
 	{
 		public static void Main (string[] args)
 		{
-			string connectionString =
-				"Server=localhost;" +
-				"Database=AskSpeakerDB;" +
-				"User ID=root;" +
-				"Password=zaq1@WSX;" +
-				"Pooling=false";
-			IDbConnection dbcon;
-			dbcon = new MySqlConnection(connectionString);
-			dbcon.Open();
-			IDbCommand dbcmd = dbcon.CreateCommand();
-			// requires a table to be created named employee
-			// with columns firstname and lastname
-			// such as,
-			//        CREATE TABLE employee (
-			//           firstname varchar(32),
-			//           lastname varchar(32));
-			string sql =
-				"SELECT * " +
-				"FROM UserRoles";
-			dbcmd.CommandText = sql;
-			IDataReader reader = dbcmd.ExecuteReader();
-			while(reader.Read()) {
-				int FirstName = (int) reader["UserRoleID"];
-				string LastName = (string) reader["RoleName"];
-				Console.WriteLine("Data: " +
-					FirstName + " " + LastName);
-			}
-			// clean up
-			reader.Close();
-			reader = null;
-			dbcmd.Dispose();
-			dbcmd = null;
-			dbcon.Close();
-			dbcon = null;
 			using(var ctx = new AskSpeakerContext()){
 				var roles = from p in ctx.UserRoles select p;
 				foreach (var r in roles) {
 					Console.WriteLine($"{r.UserRoleID} - {r.RoleName}");
 				}
-				SHA256 mySHA256 = SHA256Managed.Create();
-				Users user = new Users();
-				user.UserName = "DumbUser";
-				user.Password = mySHA256.ComputeHash (Encoding.Unicode.GetBytes("zaq1@WSX"));
-				UserRoles role = (from p in ctx.UserRoles
-				                  where p.RoleName.Equals ("Admin")
-				                  select p).FirstOrDefault ();
-				user.UserRole = role;
-				Events Event = new Events();
-				Event.EventHash = "111111";
-				Event.EventName = "Some event";
-				Event.User = user;
-				user.Events.Add(Event);
-				ctx.Users.Add (user);
-				ctx.Events.Add (Event);
-				ctx.SaveChanges();
+//				SHA256 mySHA256 = SHA256Managed.Create();
+//				Users user = new Users();
+//				user.UserName = "DumbUser";
+//				user.Password = mySHA256.ComputeHash (Encoding.Unicode.GetBytes("zaq1@WSX"));
+//				UserRoles role = (from p in ctx.UserRoles
+//				                  where p.RoleName.Equals ("Admin")
+//				                  select p).FirstOrDefault ();
+//				user.UserRole = role;
+//				Events Event = new Events();
+//				Event.EventHash = "111111";
+//				Event.EventName = "Some event";
+//				Event.User = user;
+//				user.Events.Add(Event);
+//				ctx.Users.Add (user);
+//				ctx.Events.Add (Event);
+//				ctx.SaveChanges();
 			}
+
+			WebSocketServer wss = new WebSocketServer ("ws://localhost:10000");
+			wss.AddWebSocketService<ClientRequests> ("/ClientRequest");
+			wss.Start ();
+			Console.WriteLine ("Press any key to quit...");
+			Console.ReadKey ();
+			wss.Stop ();
+
 		}
 	}
 }
